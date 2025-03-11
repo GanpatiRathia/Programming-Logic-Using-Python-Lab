0e7acc94ff0025d30af0a9a843aa554cb25ab2dc
Ganpati Rathia <gannu.edu@gmail.com>
2025-03-11 16:59:21 +0000

    Added Command Line Argument

---
+++ 08.Command-Line-arguments/argument_sum.py
@@ -0,0 +1,12 @@
+import sys
+
+sum = 0
+size_args = len(sys.argv) 
+
+len_argv = len(sys.argv)
+
+for iterator in range(1,len_argv):
+	sum += int(sys.argv[iterator])
+
+print(sum)
+
---
+++ 08.Command-Line-arguments/arguments.py
@@ -0,0 +1,6 @@
+import sys
+
+print("Number of arguments: ", len(sys.argv))
+print("Arguments: ", sys.argv)
+print("Name of the Python file is : ", sys.argv[0])
+print("Arguments: ", sys.argv[1:])
