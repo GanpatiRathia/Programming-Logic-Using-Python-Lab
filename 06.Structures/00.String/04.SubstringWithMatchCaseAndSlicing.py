881b200e9af0271213450d8e010c55e8fe081ef4
Ganpati Rathia <gannu.edu@gmail.com>
2025-03-29 04:13:41 +0000

    Added substring character no. with Match Case

--- 06.Structures/00.String/04.SubstringWithMatchCaseAndSlicing.py
+++ 06.Structures/00.String/04.SubstringWithMatchCaseAndSlicing.py
@@ -7,24 +7,30 @@ statement_length = len(statement)
 
 print("Length of the statement is : ", statement_length, end = "\n\n")
 
-for i in range(statement_length):
-	
-	match i+1 % 10 :
+# stamement = "This is a string"
+# len(statement) #16
+for i in range(statement_length): # 0 ....15
+	 #1 -> 1st
+	 #2 -> 2nd
+	 #3 -> 3rd
+	 #else -> 4th, 5th ....
+	match i+1 % 10 : #for 0th character i+1 % 10 = 1
 		case 1 :
 			print("{}st Character of {} is {}".format( i+1, statement, statement[i]) )
-	
-		case 2 :
+
+		case 2 : # i=1
 			print("{}nd Character of {} is {}".format( i+1, statement, statement[i]) )
 
-		case 3 :
+		case 3 : # i=2
 			print("{}rd Character of {} is {}".format( i+1, statement, statement[i]) )
 
-		case _ :
+		case _ : # else
 			print("{}th Character of {} is {}".format( i+1, statement, statement[i]) )
 
-b = statement[2:4]
+#statement = This is a string
+b = statement[2:4] #is
 
-print("\"" + statement + "\"" + "[2:3] is :" + b)
+print("\"" + statement + "\"" + "[2:4] is :" + b)
 
 print("\nSubstring of Hello:\n")
 print("\"Hello\"[0]","Hello"[0], end = "\n\n")
